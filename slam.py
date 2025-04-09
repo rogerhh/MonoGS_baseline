696f1031f0ff23da89ed0cc6c0e7d0ba6f72d18d
rogerhh <rogerhh@umich.edu>
2025-04-09 13:00:43 -0700

    Change datetime format

--- slam.py
+++ slam.py
@@ -235,7 +235,7 @@ if __name__ == "__main__":
 
     if config["Results"]["save_results"]:
         mkdir_p(config["Results"]["save_dir"])
-        current_datetime = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+        current_datetime = datetime.now().strftime("%Y%m%d_%H%M%S")
         path = config["Dataset"]["dataset_path"].split("/")
         save_dir = os.path.join(
             config["Results"]["save_dir"], path[-3] + "_" + path[-2], current_datetime
