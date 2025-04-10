3c21278a1802dda2aaff64bd9d35321cbb0ea42c
rogerhh <rogerhh@umich.edu>
2025-04-10 10:21:22 -0700

    Add experiment name

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -10,25 +10,36 @@ SAVE_DIR="$PROJECT_DIR/saved_runs"
 
 cd "$PROJECT_DIR"
 
-CONFIG="$PROJECT_DIR/configs/mono/tum/fr3_office.yaml"
+# CONFIG="$PROJECT_DIR/configs/mono/tum/fr3_office.yaml"
+CONFIG="$PROJECT_DIR/configs/mono/tum/fr1_desk.yaml"
 
-# Run the experiment
-python3 slam.py --config $CONFIG --eval 2>&1 | tee run.log
+num_iters=3
 
-# Check the log to see where the results are saved
-grep_str="$(grep "saving results in" run.log)"
+expname="speedup"
+for i in $(seq 1 $num_iters); do
+    # Run the experiment
+    python3 slam.py --config $CONFIG --eval 2>&1 | tee run.log
+    
+    # Check the log to see where the results are saved
+    grep_str="$(grep "saving results in" run.log)"
+    
+    # Split the grep_str by "/" and get the last part
+    datetime="${grep_str##*/}"
+    
+    RESULTS_DIR_DATETIME="$(ls -d $RESULTS_DIR/*/$datetime)"
+    
+    mkdir -p "$SAVE_DIR"
+    
+    mv run.log "$RESULTS_DIR_DATETIME/run.log"
 
-# Split the grep_str by "/" and get the last part
-datetime="${grep_str##*/}"
+    SAVE_DIR_PATH="$SAVE_DIR/${datetime}_${expname}"
 
-RESULTS_DIR_DATETIME="$(ls -d $RESULTS_DIR/*/$datetime)"
+    echo "Copying $RESULTS_DIR_DATETIME to $SAVE_DIR_PATH"
+    cp -r "$RESULTS_DIR_DATETIME" "$SAVE_DIR_PATH"
+    
+    result_wandb_path="$(ls -d $WANDB_DIR/*${datetime}*)"
+    echo "Copying ${result_wandb_path} to $SAVE_DIR_PATH"
+    cp -r ${result_wandb_path}* "$SAVE_DIR_PATH/"
+done
 
-
-mv run.log "$RESULTS_DIR_DATETIME/run.log"
-
-echo "Copying $RESULTS_DIR_DATETIME to $SAVE_DIR/$datetime"
-cp -rf "$RESULTS_DIR_DATETIME" "$SAVE_DIR/$datetime"
-
-echo "Copying $WANDB_DIR/*${datetime}* to $SAVE_DIR/$datetime"
-cp -rf $WANDB_DIR/*${datetime}* "$SAVE_DIR/$datetime"
 
